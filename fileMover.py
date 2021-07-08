25b1e84c2c66602d5936319790fd0778427bcda7
Kirill Shatalin <74788577+Phobos7K@users.noreply.github.com>
2021-07-08 11:14:54 +0300

    Update fileMover.py

--- fileMover.py
+++ fileMover.py
@@ -5,20 +5,20 @@ def MoveFiles():
     source, dest = input("Enter src path first and then dest path using space: ").split()
     ListOfFiles = os.listdir(source) #restuns a list of files located at source.
     counter = 0
-    FilesToBeMoved = [] #an empty list where we put the files that will be moved to new location.
+    FilesToBeMoved = [] 
     MostUsedExtensions = ('avi', 'ai', 'csv', 'doc', 'docx',  
                           'gif', 'htm', 'html', 'jpg', 'jpeg', 
-                          'mp3', 'mp4','mov', 'pdf', 'ppt',     #the predefined file extensions.
+                          'mp3', 'mp4','mov', 'pdf', 'ppt',     
                           'pptx', 'png', 'psd', 'rar', 'svg',
                           'txt', 'wma', 'xlsx', 'zip', '7z')
 
-    for extensions in MostUsedExtensions: print(extensions) #show all predefined extensions.
+    for extensions in MostUsedExtensions: print(extensions) 
 
     ext = input("What kind of file would you like to move?")
     if ext in MostUsedExtensions:
         ext = "." + ext
         for files in ListOfFiles:
-            if files.endswith(ext): # endswith checks the extension of a file.
+            if files.endswith(ext): 
                 counter += 1
                 FilesToBeMoved.append(files)
         print(f'---There are {counter} files ready to be moved---\n',FilesToBeMoved)
@@ -30,8 +30,8 @@ def MoveFiles():
     if MoveOrNotToMove == "y" or MoveOrNotToMove == "Y":
         for junk in FilesToBeMoved:
             shutil.move(source + junk, dest)
-            print(junk, 'was moved to the destination folder.') #This block of code moves files from Source folder
-    elif MoveOrNotToMove == "n" or MoveOrNotToMove == "N":      #to Destination Folder.
+            print(junk, 'was moved to the destination folder.') 
+    elif MoveOrNotToMove == "n" or MoveOrNotToMove == "N":      
         print("Files were not modified.")
     else:
         print("Invalid input.")
